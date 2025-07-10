c400ba5dcd9b0b6018c8949fd5977ab5cb37db34
Dipak-Gautam <anjangautam095@gmail.com>
2025-07-10 15:04:38 +0545

    feat: multipage data transfer

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def employee_details(request, pk):
+    return render(request, 'employee_detail.html')
